3f2d13baf1b3b8d3dc137a08fa26957f6741c204
Furqan Raheem Khan <50250220+FurqanRaheem@users.noreply.github.com>
2023-10-14 00:31:09 +0100

    Create Globals.cs

---
+++ CommandParserAssignmnet/Globals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandParserAssignmnet
+{
+     internal static class Globals
+    {
+        public static int pictureBoxWidth;
+        public static int pictureBoxHeight;
+        public static int startingX = 0;
+        public static int startingY = 0;
+    }
+}
