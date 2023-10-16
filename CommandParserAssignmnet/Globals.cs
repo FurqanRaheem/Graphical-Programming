2002266dda1a28d20f8a32c6ced1582c9198cd37
Furqan Raheem Khan <50250220+FurqanRaheem@users.noreply.github.com>
2023-10-16 22:29:13 +0100

    Add pictureBoxColour Property to Globals.cs

--- CommandParserAssignmnet/Globals.cs
+++ CommandParserAssignmnet/Globals.cs
@@ -10,6 +10,7 @@ namespace CommandParserAssignmnet
     {
         public static int pictureBoxWidth;
         public static int pictureBoxHeight;
+        public static Color pictureBoxColor;
         public static int startingX = 0;
         public static int startingY = 0;
     }
