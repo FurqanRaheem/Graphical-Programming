c0e54581ffc49b0260ba1077c0dc3f8efc187b4e
Furqan Raheem Khan <50250220+FurqanRaheem@users.noreply.github.com>
2023-10-22 00:54:27 +0100

    Update Parser.cs
    Add shapes commands + move refersh in here to that it displays after each command rather than all at once

--- CommandParserAssignmnet/Parser.cs
+++ CommandParserAssignmnet/Parser.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommandParserAssignmnet
 {
     public class Parser
     {
+        private Form1 form1;
         private Command command;
         private Dictionary<string, Action<string[]>> commandDictionary;
+
+        public Parser(GraphicsHandler graphicsHandler, Form1 form1)
+        {
+            this.command = new Command(graphicsHandler);
+            this.form1 = form1;
+
+            // Create a dictionary of commands and their corresponding methods
+            commandDictionary = new Dictionary<string, Action<string[]>>(StringComparer.OrdinalIgnoreCase);
+            SetCommandDictionary();
+        }
 
         public Parser(GraphicsHandler graphicsHandler)
         {
@@ -31,8 +38,21 @@ namespace CommandParserAssignmnet
             commandDictionary["reset"] = command.Reset;
             commandDictionary["pen"] = command.Pen;
             commandDictionary["fill"] = command.Fill;
+            commandDictionary["circle"] = command.Circle;
+            commandDictionary["rectangle"] = command.Rectangle;
+            commandDictionary["square"] = command.Square;
+            commandDictionary["triangle"] = command.Triangle;
+            commandDictionary["help"] = command.Help;
         }
 
+        /// <summary>
+        /// Parses a program text by splitting it into individual lines and processing each line.
+        /// </summary>
+        /// <param name="programText">The text of the program to be parsed, consisting of one or more lines.</param>
+        /// <remarks>
+        /// This method takes the provided program text and splits it into individual lines based on carriage return and line feed characters. It then processes each line using the <see cref="ParseLine"/> method.
+        /// </remarks>
+        /// <param name="programText">The text of the program to be parsed.</param>
         public void ParseProgram(string programText)
         {
             string[] lines = programText.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
@@ -40,9 +60,29 @@ namespace CommandParserAssignmnet
             foreach (string line in lines)
             {
                 ParseLine(line);
+                
+                // Add delay for "smooth" animations 
+                Thread.Sleep(Globals.animationDelay);
+
+                // Cringe, but it works
+                // In an if block for testing purposes 
+                if (form1 != null)
+                {
+                    form1.Refresh();
+                }
             }
         }
 
+        /// <summary>
+        /// Parses a single line of input, identifies the command and its parameters, and executes the corresponding action.
+        /// </summary>
+        /// <remarks>
+        /// This method takes a single line of input, splits it into individual parts using space as a delimiter, and identifies the command name.
+        /// If the command name is recognized in the <see cref="commandDictionary"/> dictionary, the corresponding action is executed with any specified parameters.
+        /// If the command is not recognized, an exception is thrown indicating the unrecognized command. The user can type 'help' to get a list of recognized commands.
+        /// </remarks>
+        /// <param name="input">The input line to be parsed, containing the command and its optional parameters.</param>
+        /// <exception cref="ArgumentException">Thrown if no command is entered or if the command is not recognized.</exception>
         public void ParseLine(string input)
         {
             string[] parts = input.Split(' ');
