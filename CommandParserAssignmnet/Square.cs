ae276c383c0c9375e9c4f2d9644fde259b2d7aae
Furqan Raheem Khan <50250220+FurqanRaheem@users.noreply.github.com>
2023-10-21 03:06:59 +0100

    Shapes - Square

---
+++ CommandParserAssignmnet/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CommandParserAssignmnet
+{
+    /// <summary>
+    /// Represents a square shape that can be drawn on a graphics surface.
+    /// </summary>
+    /// <seealso cref="CommandParserAssignmnet.Rectangle" />
+    class Square : Rectangle
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Square"/> class.
+        /// </summary>
+        public Square() : base() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Square"/> class.
+        /// </summary>
+        /// <param name="sideLength">Length of the side.</param>
+        public Square(int sideLength) : base(sideLength, sideLength) { }
+
+        /// <summary>
+        /// Draws the shape on the specified graphics surface.
+        /// </summary>
+        /// <param name="graphics">The graphics handler used to draw the shape.</param>
+        public override void Draw(GraphicsHandler graphics)
+        {
+            base.Draw(graphics);
+        }     
+    }
+}
