3995c7ff30f50b8fdde518647310317858aaddc5
Furqan Raheem Khan <50250220+FurqanRaheem@users.noreply.github.com>
2023-10-21 03:02:13 +0100

    Shapes - Interface & Class

---
+++ CommandParserAssignmnet/IShape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CommandParserAssignmnet
+{
+    /// <summary>
+    /// Represents an interface for shapes that can be drawn on a graphics surface.
+    /// </summary>
+    interface IShape
+    {
+        /// <summary>
+        /// Draws the shape on a specified graphics surface using the provided graphics handler.
+        /// </summary>
+        /// <param name="graphicsHandler">The graphics handler used to draw the shape on the surface.</param>
+        void Draw(GraphicsHandler graphicsHandler);
+    }
+}
