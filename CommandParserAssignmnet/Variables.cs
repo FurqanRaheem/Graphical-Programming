9a50df29403b89b7bbabf6d3d084e3e6e9b14dc8
Furqan Raheem Khan <50250220+FurqanRaheem@users.noreply.github.com>
2023-10-31 23:09:41 +0000

    Create Variables class using Singleton pattern

---
+++ CommandParserAssignmnet/Variables.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandParserAssignmnet
+{
+    internal class Variables
+    {
+        private static Variables instance;
+        private Dictionary<string, int> variableKeyValuePairs = new Dictionary<string, int>();
+
+        private Variables()
+        {
+            // Private constructor to prevent external instantiation.
+        }
+
+        public static Variables Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Variables();
+                }
+                return instance;
+            }
+        }
+
+        public void AddVariable(string variableName, int variableValue)
+        {
+            variableKeyValuePairs.Add(variableName, variableValue);
+        }
+
+        public int GetVariable(string variableName)
+        {
+            return variableKeyValuePairs[variableName];
+        }
+
+        public void SetVariable(string variableName, int variableValue)
+        {
+            variableKeyValuePairs[variableName] = variableValue;
+        }
+
+        public bool ContainsVariable(string variableName)
+        {
+            return variableKeyValuePairs.ContainsKey(variableName);
+        }
+
+        public void RemoveVariable(string variableName)
+        {
+            variableKeyValuePairs.Remove(variableName);
+        }
+    }
+}
