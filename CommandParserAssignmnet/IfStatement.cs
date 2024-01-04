71be61e603137c76cbdd194041df3653f6013a36
FurqanRaheem <furqanraheem10@gmail.com>
2024-01-05 01:37:54 +0800

    If Statements Class

---
+++ CommandParserAssignmnet/IfStatement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CommandParserAssignmnet
+{
+    public class IfStatement
+    {
+        private Variables variables;
+
+        public IfStatement()
+        {
+            this.variables = Variables.Instance;
+        }
+
+        public void Execute(string expression, Action<string[]> action, string[] parameters)
+        {
+            /*bool result = EvaluateExpression(expression);*/
+           /* if (result)
+            {
+                action(parameters);
+            }*/
+        }
+
+        public bool EvaluateExpression(string variableName, string comparisonOperator, int value)
+        {
+            if (variables.ContainsVariable(variableName))
+            {
+                int variableValue = variables.GetVariable(variableName);
+
+                switch (comparisonOperator)
+                {
+                    case ">":
+                        return variableValue > value;
+                    case "<":
+                        return variableValue < value;
+                    case ">=":
+                        return variableValue >= value;
+                    case "<=":
+                        return variableValue <= value;
+                    case "==":
+                        return variableValue == value;
+                    case "!=":
+                        return variableValue != value;
+
+                    default:
+                        throw new ArgumentException("Invalid comparison operator.");
+                }
+            }
+            return false; // If expression is not in the expected format or variable not found
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        public void GetCodeBlock(string input)
+        {
+            
+        }
+    }
+}
