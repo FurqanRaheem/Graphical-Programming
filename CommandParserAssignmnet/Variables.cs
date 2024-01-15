37815c297db11c0d5c5d670d91bffea56b6a95b6
FurqanRaheem <furqanraheem10@gmail.com>
2024-01-15 17:41:05 +0800

    Variables XML comments

--- CommandParserAssignmnet/Variables.cs
+++ CommandParserAssignmnet/Variables.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 
 namespace CommandParserAssignmnet
 {
+    /// <summary>
+    /// Represents a singleton class for managing variables and their values in the command parser.
+    /// </summary>  
     public class Variables
     {
         private static Variables instance;
         private Dictionary<string, int> variableKeyValuePairs = new Dictionary<string, int>();
 
-        private Variables()
-        {
-            // Private constructor to prevent external instantiation.
-        }
+        /// <summary>
+        /// Private constructor to prevent external instantiation.
+        /// </summary>
+        private Variables() {}
 
+        /// <summary>
+        /// Gets the instance of the <see cref="Variables"/> class.
+        /// </summary>
         public static Variables Instance
         {
             get
@@ -25,36 +31,68 @@ namespace CommandParserAssignmnet
             }
         }
 
+        /// <summary>
+        /// Adds a variable with the specified name and value.
+        /// </summary>
+        /// <param name="variableName">The name of the variable.</param>
+        /// <param name="variableValue">The value of the variable.</param>
         public void AddVariable(string variableName, int variableValue)
         {
             variableKeyValuePairs.Add(variableName, variableValue);
         }
 
+        /// <summary>
+        /// Gets the value of the variable with the specified name.
+        /// </summary>
+        /// <param name="variableName">The name of the variable.</param>
+        /// <returns>The value of the variable.</returns>
         public int GetVariable(string variableName)
         {
             return variableKeyValuePairs[variableName];
         }
 
+        /// <summary>
+        /// Sets the value of the variable with the specified name.
+        /// </summary>
+        /// <param name="variableName">The name of the variable.</param>
+        /// <param name="variableValue">The new value of the variable.</param>
         public void SetVariable(string variableName, int variableValue)
         {
             variableKeyValuePairs[variableName] = variableValue;
         }
 
+        /// <summary>
+        /// Checks if the dictionary contains a variable with the specified name.
+        /// </summary>
+        /// <param name="variableName">The name of the variable.</param>
+        /// <returns>True if the variable is present; otherwise, false.</returns>
         public bool ContainsVariable(string variableName)
         {
             return variableKeyValuePairs.ContainsKey(variableName);
         }
 
+        /// <summary>
+        /// Removes the variable with the specified name from the dictionary.
+        /// </summary>
+        /// <param name="variableName">The name of the variable to remove.</param>
         public void RemoveVariable(string variableName)
         {
             variableKeyValuePairs.Remove(variableName);
         }
 
+        /// <summary>
+        /// Clears all variables from the dictionary.
+        /// </summary>
         public void clearVariables()
         {
             variableKeyValuePairs.Clear();
         }
 
+        /// <summary>
+        /// Parses a variable declaration from the input and updates the dictionary accordingly.
+        /// </summary>
+        /// <param name="input">The input string representing the variable declaration.</param>
+        /// <exception cref="Exception">Thrown when there is an issue with the variable assignment.</exception>
         public void ParseDeclaration(string input)
         {
             // Split input into parts
